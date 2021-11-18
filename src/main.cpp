61c9bc8c911991b0b14e8f751cf37d984d5f22ea
pc165 <pc165@pm.me>
2021-11-18 22:58:31 +0100

    add crc32 check

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "BLESerial.h"
+#include "crc32.h"
 #include "pic.h"
 #include "pic2.h"
 #include "pic3.h"
 #include <BLEPeripheral.h>
 #include <SPI.h>
+
 #define PIN_LED1 2
 BLESerial bleSerial;
 
@@ -32,19 +34,35 @@ void setup() {
     digitalWrite(PIN_LED1, LOW);
 }
 
+void sendPicture(const uint8_t *pic, const size_t len) {
+    uint32_t code = 0;
+    uint32_t buffer = 0xFFFFFFFFu;
+    crc32(pic, len, &code);
+
+    bleSerial.write((uint8_t *)&buffer, 4);
+    bleSerial.write(code);
+
+    buffer = 0xBBBBBBBBu;
+    bleSerial.write((uint8_t *)&buffer, 4);
+    bleSerial.write(pic, len);
+    
+    buffer = 0xAAAAAAAAu;
+    bleSerial.write((uint8_t *)&buffer, 4);
+}
+
 void handleMessage() {
     if (bleSerial) {
         int byte;
         while ((byte = bleSerial.read()) > 0) {
             switch (byte) {
             case 0x01:
-                bleSerial.write(PICTURE1, PICTURE1_LEN);
+                sendPicture(PICTURE1, PICTURE1_LEN);
                 break;
             case 0x02:
-                bleSerial.write(PICTURE2, PICTURE2_LEN);
+                sendPicture(PICTURE1, PICTURE1_LEN);
                 break;
             case 0x03:
-                bleSerial.write(PICTURE3, PICTURE3_LEN);
+                sendPicture(PICTURE1, PICTURE1_LEN);
                 break;
             default:
                 break;
